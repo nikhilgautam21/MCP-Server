bdb19c1bb8e097c5a2128fb5a1806d9896d2405c
Nikhil Gautam <nikhil.gautam@ranosys.com>
2020-02-27 20:42:53 +0530

    added google-auth package

--- controllers/index.js
+++ controllers/index.js
@@ -1,65 +1,19 @@
-const { google } = require('googleapis');
-const OAuth2Data = require('../config/google_key.json')
+const google = require('../config/google_key.json')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(google.web.client_id);
 
 const googleAuthController = async (req, res, next) => {
-    try {
-        const CLIENT_ID = OAuth2Data.client.id;
-        const CLIENT_SECRET = OAuth2Data.client.secret;
-        const REDIRECT_URL = OAuth2Data.client.redirect;
-
-        const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-        var authed = false;
-
-        if (!authed) {
-            // Generate an OAuth URL and redirect there
-            const url = oAuth2Client.generateAuthUrl({
-                access_type: 'offline',
-                scope: 'https://www.googleapis.com/auth/gmail.readonly'
-            });
-            console.log(url)
-            res.redirect(url);
-        } else {
-            const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-            gmail.users.labels.list({
-                userId: 'me',
-            }, (err, res) => {
-                if (err) return console.log('The API returned an error: ' + err);
-                const labels = res.data.labels;
-                if (labels.length) {
-                    console.log('Labels:');
-                    labels.forEach((label) => {
-                        console.log(`- ${label.name}`);
-                    });
-                } else {
-                    console.log('No labels found.');
-                }
-            });
-            res.send('Logged in')
-        }
-
-    } catch (e) {
-        console.log(e)
-        res.send(e)
-    }
+    console.log(req.headers)
+    const ticket = await client.verifyIdToken({
+        idToken: req.headers.bearer,
+        audience: google.web.client_id
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    res.send(userid)
 }
 
 const googleAuthCallBackController = async (req, res, next) => {
-
-    const code = req.query.code
-    if (code) {
-        // Get an access token based on our OAuth code
-        oAuth2Client.getToken(code, function (err, tokens) {
-            if (err) {
-                console.log('Error authenticating')
-                console.log(err);
-            } else {
-                console.log('Successfully authenticated');
-                oAuth2Client.setCredentials(tokens);
-                authed = true;
-                res.redirect('/')
-            }
-        });
-    }
 
 }
 
